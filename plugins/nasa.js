368a00f4bf0ddcff43461d4d6ea1aebe39d82007
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-07-22 02:06:10 +0530

    Add files via upload

--- plugins/nasa.js
+++ plugins/nasa.js
@@ -3,10 +3,11 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, text, command }) => {
 const res = await fetch("https://darkapi--technicalhacke4.repl.co/nasanews");
 let tech = await res.json()
+const img = `${tech.result.image}`
 
 let aliveMessage = {
                 image: {
-                    url: "${tech.result.image}",
+                    url: img,
                 },
                 caption: `*◈━━━━━━━━━━━━━◈*   *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*\n*◈━━━━━━━━━━━━━◈*\n\n🛰️𝙏𝙄𝙏𝙇𝙀🛰️_ *${tech.result.title}*\n\n🛰️𝙉𝙀𝙒𝙎🛰️_ ${tech.result.decs}\n\n\n${tech.result.link}\n🔮🚔*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*🚔🔮`,
                 footer: `*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*`,
