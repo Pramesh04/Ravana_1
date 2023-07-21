05a40102ccaf8b604546c2c87e399b2e8f7be02c
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-07-22 03:02:51 +0530

    Add files via upload

--- plugins/nasa.js
+++ plugins/nasa.js
@@ -4,15 +4,16 @@ let handler = async (m, { conn, usedPrefix, text, command }) => {
 const res = await fetch("https://darkapi--technicalhacke4.repl.co/nasanews");
 let tech = await res.json()
 const img = `${tech.result.image}`
-
+const desc = `${tech.result.decs}`
 let aliveMessage = {
                 image: {
                     url: img,
                 },
-                caption: `*◈━━━━━━━━━━━━━◈*   *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*\n*◈━━━━━━━━━━━━━◈*\n\n🛰️𝙏𝙄𝙏𝙇𝙀🛰️_ *${tech.result.title}*\n\n🛰️𝙉𝙀𝙒𝙎🛰️_ ${tech.result.decs}\n\n\n${tech.result.link}\n🔮🚔*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*🚔🔮`,
+                caption: `*◈━━━━━━━━━━━━━◈*   *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*\n*◈━━━━━━━━━━━━━◈*\n\n🛰️𝙏𝙄𝙏𝙇𝙀🛰️_ *${tech.result.title}*\n\n🛰️𝙉𝙀𝙒𝙎🛰️_`+`${desc}`+`\n\n\n${tech.result.link}\n🔮🚔*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*🚔🔮`,
                 footer: `*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*`,
                 headerType: 4,
             };
+            m.reply("*🛸serching🛸*\n⬇️please wait⬇️")
 await conn.sendMessage(m.chat, aliveMessage ,m)
 }
 
