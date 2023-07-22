b91605588be92f34205d46bedbe37ba7b93ba407
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-07-22 19:23:23 +0530

    Update hirunews.js

--- plugins/hirunews.js
+++ plugins/hirunews.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, usedPrefix, text, command }) => {
 const aluth = await fetch("https://darkapi--technicalhacke4.repl.co/hirunews");
 const bijjo = aluth.json()
 
-const images = `${hirunews.image}`
-           const title = `${hirunews.title}`
-           const date = `${hirunews.time}`
-           const news = `${hirunews.desc}`
+const images = `${hirunews.result.image}`
+           const title = `${hirunews.result.title}`
+           const date = `${hirunews.result.time}`
+           const news = `${hirunews.result.desc}`
 let aliveMessage = {
                 image: {
-                    url: `${images}`,
+                    url: ${images},
                 },
                 caption: `*◈━━━━━━━━━━━━━◈*   *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*\n*◈━━━━━━━━━━━━━◈*\n\n☀𝙏𝙄𝙏𝙇𝙀☀_ *${title}*\n\n☀𝙉𝙀𝙒𝙎☀_ ${news}\n\n⌛𝙳𝙰𝚃𝙴⌛ _${date}_`,
                 footer: `*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*`,
