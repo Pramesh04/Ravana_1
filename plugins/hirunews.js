30caec999466cdd65d317011da108ff942882995
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-07-23 03:35:59 +0530

    Add files via upload

--- plugins/hirunews.js
+++ plugins/hirunews.js
@@ -3,24 +3,20 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, text, command }) => {
 const aluth = await fetch("https://darkapi--technicalhacke4.repl.co/hirunews");
 const hirunews = aluth.json()
-
-           const images = `${hirunews.result.image}`
-           const title = `${hirunews.result.title}`
-           const date = `${hirunews.result.time}`
-           const news = `${hirunews.result.desc}`
+const images = `${hirunews.result.image}`
 let aliveMessage = {
                 image: {
                     url: ${images},
                 },
-                caption: `*◈━━━━━━━━━━━━━◈*   *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*\n*◈━━━━━━━━━━━━━◈*\n\n☀𝙏𝙄𝙏𝙇𝙀☀_ *${title}*\n\n☀𝙉𝙀𝙒𝙎☀_ ${news}\n\n⌛𝙳𝙰𝚃𝙴⌛ _${date}_`,
+                caption: `*◈━━━━━━━━━━━━━◈*   *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*\n*◈━━━━━━━━━━━━━◈*\n\n☀𝙏𝙄𝙏𝙇𝙀☀_ \n*${hirunews.result.title}*\n\n☀𝙉𝙀𝙒𝙎☀_ \n${hirunews.result.desc}\n\n\n_${hirunews.result.time}_`,
                 footer: `*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*`,
                 headerType: 4,
             };
-await conn.sendMessage(m.chat, aliveMessage ,m)
+conn.sendMessage(m.chat, aliveMessage ,m)
 }
 
 
-handler.help = ['hirunews']
+handler.help = ['hirunews','hn']
 handler.tags = ['main']
 handler.command = /^(hirunews|hn)$/i
 handler.premium = true
