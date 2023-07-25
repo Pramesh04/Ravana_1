7d716477f2faed6f154c1a4ffa70176bbcb0995c
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-07-25 20:09:27 +0530

    Add files via upload

---
+++ plugins/dl-play.js
@@ -0,0 +1,35 @@
+
+import yts from 'yt-search'
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+	
+	if (!text) throw `✳️ Ingresa el título de una canción\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life`
+	let res = await yts(text)
+	let vid = res.videos[0]
+	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
+	//const url = 'https://www.youtube.com/watch?v=' + videoId
+
+	let play = `
+◈━━━━━━━━━━━━━◈
+     *𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*
+◈━━━━━━━━━━━━━◈
+
+📃 *Título* : ${title}
+
+📆 *Publicado:* ${ago}
+
+⌚ *Duración:* ${timestamp}
+
+👀 *Vistas:* ${views}
+◈━━━━━━━━━━━━━◈`
+ await conn.sendButton(m.chat, play, fgig, thumbnail, [
+    ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
+    ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
+  ], m, rpl)
+}
+handler.help = ['splay']
+handler.tags = ['dl']
+handler.command = ['play', 'playvid']
+handler.disabled = true
+
+export default handler
