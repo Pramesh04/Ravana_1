a7d0baa3cf9124b5caa9e61a046099e6ec8d5203
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-07-30 12:43:02 +0530

    Update hirunews.js

--- plugins/hirunews.js
+++ plugins/hirunews.js
@@ -14,6 +14,7 @@ let aliveMessage = {
                 footer: `*𝗞𝙸𝙽𝙶 𝗥𝙰𝚅𝙰𝙽𝙰 𝗠𝗗*`,
                 headerType: 4,
             };
+await conn.sendMessage(m.chat, { text: `*⏳𝙋𝙇𝘼𝙎𝙀 𝙒𝘼𝙄𝙏⏳*` } ,m)
 await conn.sendMessage(m.chat, aliveMessage ,m)
 }
 
